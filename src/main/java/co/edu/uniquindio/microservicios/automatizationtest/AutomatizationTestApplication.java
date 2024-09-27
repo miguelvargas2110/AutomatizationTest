38e3c34dd4b76f0f0faac476a6dcd66a555a8dc9
Miguel Angel Vargas Navas <105131881+miguelvargas2110@users.noreply.github.com>
2024-09-27 11:32:17 -0500

    Pruebas automatizadas con cucumber reports creadas

---
+++ src/main/java/co/edu/uniquindio/microservicios/automatizationtest/AutomatizationTestApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.microservicios.automatizationtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatizationTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutomatizationTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/edu/uniquindio/microservicios/automatizationtest/AutomatizationTestApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.microservicios.automatizationtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomatizationTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
